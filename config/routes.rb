30f7212a3d8f37f3d47ff543cba5063a6d3600b5
Justin Ndwiga <ndwigajustin@gmail.com>
2017-06-09 15:54:16 +0300

    Login/Reg form

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :movies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :movies, only: [:show, :index]
-  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-
-
   root 'movies#index'
-
+  resources :movies, only: [:show, :index]
 
 end
