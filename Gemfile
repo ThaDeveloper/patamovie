674f7a9dd371b65bd767d5c0d41ad814ac9f4443
Justin Ndwiga <ndwigajustin@gmail.com>
2017-06-09 10:00:53 +0300

    Install and configure Devise

--- Gemfile
+++ Gemfile
@@ -54,3 +54,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 #Foundation styling framework
 gem 'foundation-rails'
+
+#User authentication
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :movies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies, only: [:show, :index]
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
 
   root 'movies#index'
