fefbdad5c2b9c71f6c57f19add08709dbfeeed3a
Justin Ndwiga <ndwigajustin@gmail.com>
2017-06-10 14:23:58 +0300

    Header styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,15 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
+(function($) {
+  $(function() {
+    $('nav ul li > a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      $('.nav-dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
+    });
+  });
+})(jQuery);
